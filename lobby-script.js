fbba8f3e25f4dc8e624a55d5bea0f3d8a806aba6
gameno1-admin <kappelermerlin@gmail.com>
2024-12-28 21:49:31 +0100

    Revealed Word List fix

--- lobby-script.js
+++ lobby-script.js
@@ -150,7 +150,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Spieler hat ein Wort revealed
-    socket.on('playerRevealed', ({ playerId, word}) => {
+    socket.on('playerRevealed', ({ playerId }) => {
         console.log(`Spieler revealed: ${playerId}`);
         const playerElements = document.querySelectorAll('.player');
         playerElements.forEach(playerElement => {
@@ -169,7 +169,7 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(`revealedPlayers.length: ${revealedCount}, totalPlayers: ${totalPlayers}`);
             if (revealedCount === totalPlayers) {
                 console.log('Alle Spieler haben revealed!');
-                socket.emit('evaluateAnswers', word); // Sende Event zur Auswertung der Antworten
+                socket.emit('evaluateAnswers'); // Sende Event zur Auswertung der Antworten
             }
         })
     });
